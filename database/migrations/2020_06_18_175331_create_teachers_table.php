9fc2a57e494a080194d326ea381ef2a1065fe3fd
YAHMIS YAHAYA <yahayamiss@gmail.com>
2020-06-18 22:18:08 +0100

    Migration
    Created migration database and run migration

---
+++ database/migrations/2020_06_18_175331_create_teachers_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->bigIncrements('teacher_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('father_name');
+            $table->string('mother_name');
+            $table->string('gender');
+            $table->string('email')->unique();
+            $table->date('dob');
+            $table->string('phone');
+            $table->longText('address');
+            $table->string('nationality');
+            $table->string('passport');
+            $table->tinyInteger('status');
+            $table->date('dateregistered');
+            $table->integer('user_id');
+            // $table->integer('semeter_id');
+            $table->string('image')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
